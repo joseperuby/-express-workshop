2437cca21ffca1bfe66eb0e813d7b0d98f279910
JoseLuis <peperala34@gmail.com>
2024-09-08 21:53:28 -0600

    Activity 5. POST requests, ternary operator, filter method and body parser.

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
 const express = require('express');
 const app = express();
-const {pokemon} = require('./pokedex.json')
+const {pokemon} = require('./pokedex.json');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res, next) => {
-    res.send("Bienvenido al Pokedex");
-    res.status(200);
+    return res.status(200).send("Bienvenido al Pokedex");
 });
 
 app.get("/pokemon/all", (req,res, next) => {
-    res.status(200);
-    res.send(pokemon);
+    return res.status(200).send(pokemon);
 });
 
-app.get('/pokemon/:name', (req, res, next) => {
+app.get('/pokemon/:name([A-Za-z]+)', (req, res, next) => {
     const name = req.params.name;
-    for(i=0; i<pokemon.length; i++){
-        if(pokemon[i].name == name){
-            res.status(200);
-            res.send(pokemon[i]);
-        }
-    }
-    res.status(404);
-    res.send("Pokemon no encontrado");
+    const pk = pokemon.filter((p) => {
+        return (p.name.toUpperCase() == name.toUpperCase()) ? p : null;
+    });
+    console.log(pk);
+
+    (pk.length > 0) ? res.status(202).send(pk) : res.status(404).send("Pokemon no encontrado");
 });
+
+
 
 app.get('/pokemon/:id([0-9{1,3}])', (req, res, next) => {
     const id = req.params.id - 1
     if(id >= 0 && id <= 150){
-        res.status(200);
-        res.send(pokemon[req.params.id - 1]);
+        return res.status(200).send(pokemon[req.params.id - 1]);
     }
     else{
-        res.status(404),
-        res.send("Pokemon no encontrado")
+        return res.status(404).send("Pokemon no encontrado")
     }
 });
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+app.post("/pokemon", (req, res, next) => {
+    return  res.status(200).send(req.body);
+})
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running")
